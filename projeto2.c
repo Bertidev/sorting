c76a594038582a13faf6c6157e3eaf19cc19ebc2
Bertidev <111362675+Bertidev@users.noreply.github.com>
2023-11-23 16:53:57 -0300

    Add files via upload
    versao final

--- projeto2.c
+++ projeto2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 
-typedef struct informacao 
+typedef struct informacao
 {
     unsigned long int key;
     float num;
@@ -37,7 +37,7 @@ info* create_vet (unsigned long int size, int seed)
     return v;
 }
 
-void InsertionSort(info* array, long int size)
+void InsertionSort (info* array, long int size)
 {
     long int i, j;
     unsigned long int element;
@@ -46,7 +46,7 @@ void InsertionSort(info* array, long int size)
     {
         element = array[i].key;
         j = i - 1;
-        while (j >= 0 && array[j].key < element) 
+        while (j >= 0 && array[j].key < element)
         {
             array[j + 1].key = array[j].key;
             j = j - 1;
@@ -55,38 +55,38 @@ void InsertionSort(info* array, long int size)
     }
 }
 
-void swap(info* array, long int i, long int j) 
-{ 
-    unsigned long int element = array[i].key; 
-    array[i].key = array[j].key; 
-    array[j].key = element; 
-} 
-  
-void BubbleSort(info* array, long int size) 
-{ 
-    long int i, j; 
-    for (i = 0; i < size - 1; i++) 
+void swap (info* array, long int i, long int j)
+{
+    unsigned long int element = array[i].key;
+    array[i].key = array[j].key;
+    array[j].key = element;
+}
+
+void BubbleSort (info* array, long int size)
+{
+    long int i, j;
+    for (i = 0; i < size - 1; i++)
     {
         for (j = 0; j < size - i - 1; j++)
-        { 
+        {
             if (array[j].key < array[j + 1].key)
-            { 
+            {
                 swap(array, j, j + 1);
             }
         }
-    } 
+    }
 }
 
-void ShellSort(info* array, long int size) 
+void ShellSort (info* array, long int size)
 {
     long int interval, i, j;
     unsigned long int element;
-    for (interval = size / 2; interval > 0; interval /= 2) 
+    for (interval = size / 2; interval > 0; interval /= 2)
     {
-        for (i = interval; i < size; i += 1) 
+        for (i = interval; i < size; i += 1)
         {
             element = array[i].key;
-            for (j = i; j >= interval && array[j - interval].key < element; j -= interval) 
+            for (j = i; j >= interval && array[j - interval].key < element; j -= interval)
             {
                 array[j].key = array[j - interval].key;
             }
@@ -95,7 +95,7 @@ void ShellSort(info* array, long int size)
     }
 }
 
-unsigned long int GetMax(info* array, long int size) 
+unsigned long int GetMax (info* array, long int size)
 {
     unsigned long int max = array[0].key;
 
@@ -109,12 +109,12 @@ unsigned long int GetMax(info* array, long int size)
     return max;
 }
 
-void CountingSort(info* array, long int size, long int place, info* output) 
+void CountingSort (info* array, long int size, long int place, info* output)
 {
     unsigned long int max = array[0].key % 10;
     unsigned long int i;
 
-    for (i = 1; i < (unsigned long int)size; i++) 
+    for (i = 1; i < (unsigned long int)size; i++)
     {
         if ((array[i].key % 10) > max)
         {
@@ -126,7 +126,6 @@ void CountingSort(info* array, long int size, long int place, info* output)
 
     if (count == NULL)
     {
-        // Tratar erro de alocação de memória, se necessário
         return;
     }
 
@@ -138,40 +137,34 @@ void CountingSort(info* array, long int size, long int place, info* output)
     for (i = 0; i < (unsigned long int)size; i++)
     {
         count[(array[i].key / place) % 10]++;
-    }    
+    }
 
     for (i = max; i > 0; i--)
     {
         count[i - 1] += count[i];
     }
-  
-    for (i = size - 1; (long int)i >= 0; i--) 
+
+    for (i = size - 1; (long int)i >= 0; i--)
     {
         output[count[(array[i].key / place) % 10] - 1] = array[i];
         count[(array[i].key / place) % 10]--;
     }
 
-    // Copia os elementos ordenados de volta para o array original
     for (int i = 0; i < size; i++)
     {
         array[i] = output[i];
     }
 
-    // Libera a memória alocada para o vetor count
     free(count);
 }
 
-
-void RadixSort(info* array, long int size) 
+void RadixSort (info* array, long int size)
 {
     unsigned long int max = GetMax(array, size);
-
-    // Aloca memória para o vetor de saída
     info* output = (info*)malloc(size * sizeof(info));
 
-    if (output == NULL) 
+    if (output == NULL)
     {
-        // Tratar erro de alocação de memória, se necessário
         return;
     }
 
@@ -181,18 +174,15 @@ void RadixSort(info* array, long int size)
         CountingSort(array, size, i, output);
     }
 
-    // Copia os elementos ordenados de volta para o array original
     for (int i = 0; i < size; i++)
     {
         array[i] = output[i];
     }
 
-    // Libera a memória alocada para o vetor de saída
     free(output);
 }
 
-
-void merge(info* array, info* c, long int i, long int m, long int f)
+void merge (info* array, info* c, long int i, long int m, long int f)
 {
     long int z, iv=i, ic=m+1;
     for(z=i; z<= f; z++)
@@ -202,7 +192,7 @@ void merge(info* array, info* c, long int i, long int m, long int f)
     z = i;
     while(iv<=m && ic<= f)
     {
-        if(c[iv].key >= c[ic].key) 
+        if(c[iv].key >= c[ic].key)
         {
             array[z++].key = c[iv++].key;
         }
@@ -216,68 +206,238 @@ void merge(info* array, info* c, long int i, long int m, long int f)
     {
         array[z++].key = c[iv++].key;
     }
-    
+
     while(ic <= f)
     {
         array[z++].key = c[ic++].key;
     }
 }
 
-void sort(info* array, info* c, long int i, long int f)
+void sort (info* array, info* c, long int i, long int f)
 {
     if(i < f)
     {
         long int m = (i+f)/2;
         sort(array,c,i,m);
         sort(array,c,m+1,f);
-        if(array[m].key < array[m+1].key) 
+        if(array[m].key < array[m+1].key)
         {
             merge(array,c,i,m,f);
         }
     }
 }
 
-void MergeSort(info* array, long int size)
+void MergeSort (info* array, long int size)
 {
     info* c = malloc(sizeof(info)*size);
     sort(array,c,0,size-1);
     free(c);
 }
 
-long int partition(info* array, long int low, long int high, long int mid) 
+long int partitionlower (info *array, long int LI, long int LS)
 {
-    unsigned long int pivot = array[mid].key;
+    info aux;
+    unsigned long int pivot;
+    long int e = LI, d = LS;
+    pivot = array[e].key;
 
-    swap(array, mid, high);
-
-    long int i = low - 1;
-
-    for (long int j = low; j <= high - 1; j++) 
+    while(e<d)
     {
-        if (array[j].key >= pivot) 
+        while(array[e].key >= pivot && (e<LS))
         {
-            i++;
-            swap(array, i, j);
+            e++;
+        }
+        while(array[d].key < pivot && (d>LI))
+        {
+            d--;
+        }
+        if(e<d)
+        {
+            aux = array[e];
+            array[e] = array[d];
+            array[d] = aux;
+
         }
     }
-    swap(array, i + 1, high);
-    return (i + 1);
+
+    aux = array[LI];
+    array[LI] = array[d];
+    array[d] = aux;
+
+    return d;
 }
 
-void QuickSort(info* array, long int low, long int high) 
+long int partition (info *array, long int LI, long int LS)
 {
-    if (low < high) 
-    {
-        long int mid = (low + high) / 2;
+    info aux;
+    unsigned long int pivot;
+    long int e = LI, d = LS;
+    long int meio;
 
-        long int pi = partition(array, low, high, mid);
-        
-        QuickSort(array, low, pi - 1);
-        QuickSort(array, pi + 1, high);
+    meio = (LI + LS) / 2;
+    pivot = array[meio].key;
+
+    while (e <= d)
+    {
+        while (array[e].key > pivot)
+        {
+            e++;
+        }
+        while (array[d].key < pivot)
+        {
+            d--;
+        }
+        if (e <= d)
+        {
+            aux = array[e];
+            array[e] = array[d];
+            array[d] = aux;
+            e++;
+            d--;
+        }
+    }
+
+    return e;
+}
+
+long int partitionupper (info *array, long int LI, long int LS)
+{
+    info aux;
+    unsigned long int pivot;
+    long int e = LI, d = LS;
+    pivot = array[d].key;
+
+    while(e<d)
+    {
+        while(array[e].key > pivot && (e<LS))
+        {
+            e++;
+        }
+        while(array[d].key <= pivot && (d>LI))
+        {
+            d--;
+        }
+        if(e<d)
+        {
+            aux = array[e];
+            array[e] = array[d];
+            array[d] = aux;
+
+        }
+    }
+
+    aux =array[LI];
+    array[LI] = array[d];
+    array[d] = aux;
+
+    return d;
+}
+
+void quicksortlower (info *array, long int LI, long int LS)
+{
+    while (LI < LS)
+    {
+        long int p = partitionlower(array, LI, LS);
+        if  (p- LI < LS - p)
+        {
+            quicksortlower(array, LI, p-1);
+            LI = p +1;
+        }
+        else
+        {
+            quicksortlower(array, p+1, LS);
+            LS = p-1;
+        }
     }
 }
 
-void unordered(long int size, int seed) 
+void QuickSort (info *array, long int LI, long int LS)
+{
+    if (LI < LS)
+    {
+        long int p = partition(array, LI, LS);
+        QuickSort(array, LI, p - 1);
+        QuickSort(array, p, LS);
+    }
+}
+
+void quicksortupper (info *array, int LI, int LS)
+{
+    while (LI < LS)
+    {
+        long int p = partitionupper(array, LI, LS);
+        if  (p- LI < LS - p)
+        {
+            quicksortupper(array, LI, p-1);
+            LI = p +1;
+        }
+        else
+        {
+            quicksortupper(array, p+1, LS);
+            LS = p-1;
+        }
+    }
+}
+
+void quickrandom(long int size, int seed)
+{
+    struct timespec begin, end;
+    printf("VETOR DE TAMANHO %ld\n",size);
+    info* vetor = create_vet(size,seed);
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    quicksortlower(vetor,0,size-1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double lower = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
+    free(vetor);
+    printf("\tPivo LI: %f\n", lower);
+
+    info* vetor2 = create_vet(size,seed);
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    QuickSort(vetor2,0,size-1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double middle = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
+    free(vetor2);
+    printf("\tPivo meio: %f\n", middle);
+
+    info* vetor3 = create_vet(size,seed);
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    quicksortupper(vetor3,0,size-1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double upper = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
+    free(vetor3);
+    printf("\tPivo LS: %f\n\n", upper);
+}
+
+void quickordered(long int size, int seed)
+{
+    struct timespec begin, end;
+    printf("VETOR DE TAMANHO %ld\n",size);
+    info* vetor = create_ord(size,seed);
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    quicksortlower(vetor,0,size-1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double lower = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
+    free(vetor);
+    printf("\tPivo LI: %f\n", lower);
+
+    info* vetor2 = create_ord(size,seed);
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    QuickSort(vetor2,0,size-1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double middle = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
+    free(vetor2);
+    printf("\tPivo meio: %f\n", middle);
+
+    info* vetor3 = create_ord(size,seed);
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    quicksortupper(vetor3,0,size-1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double upper = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
+    free(vetor3);
+    printf("\tPivo LS: %f\n", upper);
+}
+
+void unordered(long int size, int seed)
 {
     struct timespec begin, end;
 
@@ -287,7 +447,7 @@ void unordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double insert = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array);
-    printf("\ttempo da insertionsort: %f\n", insert);
+    printf("\tTempo da insertionsort: %f\n", insert);
 
     info* array2 = create_vet(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -295,7 +455,7 @@ void unordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double bubble = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array2);
-    printf("\ttempo da bubblesort: %f\n", bubble);
+    printf("\tTempo da bubblesort: %f\n", bubble);
 
     info* array3 = create_vet(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -303,7 +463,7 @@ void unordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double shell = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array3);
-    printf("\ttempo da shellsort: %f\n", shell);
+    printf("\tTempo da shellsort: %f\n", shell);
 
     info* array4 = create_vet(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -311,7 +471,7 @@ void unordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double radix = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array4);
-    printf("\ttempo da radixsort: %f\n", radix);
+    printf("\tTempo da radixsort: %f\n", radix);
 
     info* array5 = create_vet(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -319,7 +479,7 @@ void unordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double quick = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array5);
-    printf("\ttempo da quicksort: %f\n", quick);
+    printf("\tTempo da quicksort: %f\n", quick);
 
     info* array6 = create_vet(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -327,7 +487,7 @@ void unordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double merge = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array6);
-    printf("\ttempo da mergesort: %f\n", merge);
+    printf("\tTempo da mergesort: %f\n", merge);
 }
 
 void all_unordered (long int size)
@@ -352,10 +512,10 @@ void all_unordered (long int size)
     unordered(size,44);
     printf("Tipo 1 com tamanho %ld (10): \n",size);
     unordered(size,24);
-    printf("Sorting dos vetores de tamanho %ld concluido, aperte enter para continuar",size);
+    printf("Sorting dos vetores de tamanho %ld concluido\n\n",size);
 }
 
-void ordered(long int size, int seed) 
+void ordered(long int size, int seed)
 {
     struct timespec begin, end;
 
@@ -365,7 +525,7 @@ void ordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double insert = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array);
-    printf("\ttempo da insertionsort: %f\n", insert);
+    printf("\tTempo da insertionsort: %f\n", insert);
 
     info* array2 = create_ord(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -373,7 +533,7 @@ void ordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double bubble = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array2);
-    printf("\ttempo da bubblesort: %f\n", bubble);
+    printf("\tTempo da bubblesort: %f\n", bubble);
 
     info* array3 = create_ord(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -381,7 +541,7 @@ void ordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double shell = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array3);
-    printf("\ttempo da shellsort: %f\n", shell);
+    printf("\tTempo da shellsort: %f\n", shell);
 
     info* array4 = create_ord(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -389,7 +549,7 @@ void ordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double radix = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array4);
-    printf("\ttempo da radixsort: %f\n", radix);
+    printf("\tTempo da radixsort: %f\n", radix);
 
     info* array5 = create_ord(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -397,7 +557,7 @@ void ordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double quick = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array5);
-    printf("\ttempo da quicksort: %f\n", quick);
+    printf("\tTempo da quicksort: %f\n", quick);
 
     info* array6 = create_ord(size, seed);
     clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -405,7 +565,7 @@ void ordered(long int size, int seed)
     clock_gettime(CLOCK_MONOTONIC, &end);
     double merge = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1e9;
     free(array6);
-    printf("\ttempo da mergesort: %f\n", merge);
+    printf("\tTempo da mergesort: %f\n", merge);
 }
 
 void all_ordered (long int size)
@@ -430,11 +590,27 @@ void all_ordered (long int size)
     ordered(size,90);
     printf("Tipo 2 com tamanho %ld (10): \n",size);
     ordered(size,33);
-    printf("Sorting dos vetores de tamanho %ld concluido, aperte enter para continuar",size);
+    printf("Sorting dos vetores de tamanho %ld concluido\n\n",size);
 }
 
 int main ()
 {
+    system("cls");
+    printf("Teste para escolha do pivo para a quicksort com vetor aleatorio: \n");
+    quickrandom(10000,13);
+    quickrandom(50000,13);
+    quickrandom(100000,13);
+    quickrandom(500000,13);
+    quickrandom(1000000,13);
+
+    printf("\nTeste para escolha do pivo para a quicksort com vetor ordenado: \n");
+    quickordered(10000,13);
+    quickordered(50000,13);
+    quickordered(100000,13);
+    quickordered(500000,13);
+    quickordered(1000000,13);
+    printf("\n");
+
     all_unordered(10000);
     all_unordered(50000);
     all_unordered(100000);
@@ -446,6 +622,6 @@ int main ()
     all_ordered(100000);
     all_ordered(500000);
     all_ordered(1000000);
-    
+
     return 0;
 }
